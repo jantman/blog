a31d9e962ae03fb5d5ad5ea7577bbaf11c3f878a
Jason Antman <jason@jasonantman.com>
2014-01-10 20:12:38 -0500

    regenerated

--- pelicanconf.py
+++ pelicanconf.py
@@ -25,7 +25,7 @@ LINKS =  (('Pelican', 'http://getpelican.com/'),
 SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
-DEFAULT_PAGINATION = False
+DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
